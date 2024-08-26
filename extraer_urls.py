7f9843296bb3947375c4863c2deab106054832a4
Mauricio Urdaneta <mauriurdaneta20@gmail.com>
2024-08-26 16:48:36 -0500

    nuevo ejercicio agregado lector de urls

---
+++ extraer_urls.py
@@ -0,0 +1,8 @@
+import re
+e = r"(https?)://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
+text="Visita mi blog en https://www.miblog.com o sigue mi perfil en http://twitter.com/usuario"
+
+match = re.findall(e,text)
+
+
+print(match)
